0a700e91783824ad2f89d6aa76c1846135389159
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-06-30 21:08:32 +0700

    update

--- keyboards/mechlovin/packman65/config.h
+++ keyboards/mechlovin/packman65/config.h
@@ -49,5 +49,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BACKLIGHT_PIN B8
 #define BACKLIGHT_BREATHING
-#define BACKLIGHT_PWM_DRIVER PWMD8
-#define BACKLIGHT_PWM_CHANNEL 2
+#define BACKLIGHT_PWM_DRIVER PWMD4
+#define BACKLIGHT_PWM_CHANNEL 3
