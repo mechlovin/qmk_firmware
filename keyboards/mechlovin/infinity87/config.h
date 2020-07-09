31fc4eb36fb6aa872656fea7e0f880df495eac0a
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-07-09 19:42:55 +0700

    add

--- keyboards/mechlovin/infinity87/config.h
+++ keyboards/mechlovin/infinity87/config.h
@@ -21,11 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID   0x4D4C // ML
-#define PRODUCT_ID  0x8701
-#define DEVICE_VER  0x0001
-#define MANUFACTURER  Team.Mechlovin
-#define PRODUCT       Infinity 87
-#define DESCRIPTION   Team.Mechlovin Infinity 87
 
 /* key matrix size */
 #define MATRIX_ROWS 6
---
+++ keyboards/mechlovin/infinity87/rev2/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define PRODUCT_ID  0x8702
+#define DEVICE_VER  0x0001
+#define MANUFACTURER  Team.Mechlovin
+#define PRODUCT       Infinity 87 rev2
+#define DESCRIPTION   Team.Mechlovin Infinity 87 rev2
