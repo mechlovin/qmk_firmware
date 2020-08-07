2bb8640fed65b723e7267052178c7d27eb67e0a1
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-08-07 17:10:24 +0700

    add ori87 PCB

---
+++ keyboards/mechlovin/infinity87/standard/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define PRODUCT_ID  0x8701
+#define DEVICE_VER  0x0001
+#define PRODUCT       Infinity 87
+
+#define RGB_DI_PIN A15
+#define RGBLED_NUM 26
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_ANIMATIONS
