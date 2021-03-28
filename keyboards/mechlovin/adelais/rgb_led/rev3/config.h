fbf3efb14bc7d2e57b0fbb403b77fb44fd4c64c6
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-03-28 13:32:09 +0700

    add adelasi rgb rev3

--- keyboards/mechlovin/adelais/rgb_led/rev3/config.h
+++ keyboards/mechlovin/adelais/rgb_led/rev3/config.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define PRODUCT_ID   0xAEC2
-#define PRODUCT      Adelais En Ciel Rev2  
+#define PRODUCT_ID   0xAEC3
+#define PRODUCT      Adelais En Ciel Rev3
 
-#define MATRIX_ROW_PINS { B1, A0, C13, A1, A2}
-#define MATRIX_COL_PINS { A10, A9, A8, B15, B14, B13, B12, B11, B10, B8, B4, B5, B3, C14, A7 }
+#define MATRIX_ROW_PINS { A4, A3, C13, A1, A2}
+#define MATRIX_COL_PINS { B10, B11, B2, B1, B0, A6, A5, A14, A13, A15, B3, B4, B5, B8, B9 }
 
-#define RGB_DI_PIN A15
+#define RGB_DI_PIN A7
+#define WS2812_SPI SPID1 // default: SPID1
+#define WS2812_SPI_MOSI_PAL_MODE 5 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
 #define RGBLED_NUM      23
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
@@ -21,30 +23,29 @@
 // 0b1110111 AD <-> VCC
 // 0b1110101 AD <-> SCL
 // 0b1110110 AD <-> SDA
-#define DRIVER_ADDR_1 0b1110100
-#define DRIVER_ADDR_2 0b1110110
-#define DRIVER_COUNT 2
-#define DRIVER_1_LED_TOTAL 32
-#define DRIVER_2_LED_TOTAL 36
-#define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+#define DRIVER_ADDR_1 0b0110010
+#define DRIVER_ADDR_2 0b0110010
+#define DRIVER_COUNT 1
+#define DRIVER_1_LED_TOTAL 66
+#define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
 #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 #define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_PINWHEEL // Sets the default mode, if none has been set
 
 #define RGB_MATRIX_DISABLE_KEYCODES
 
-#define ENCODERS_PAD_A { A4 }
-#define ENCODERS_PAD_B { A3 }
+#define ENCODERS_PAD_A { B12 }
+#define ENCODERS_PAD_B { B13 }
 #define ENCODER_RESOLUTION 4
 
 #define TAP_CODE_DELAY 10
 
 
-#define LED_NUM_LOCK_PIN C15
-#define LED_CAPS_LOCK_PIN B2
-#define LED_SCROLL_LOCK_PIN B9
+#define LED_NUM_LOCK_PIN A0
+#define LED_CAPS_LOCK_PIN C14
+#define LED_SCROLL_LOCK_PIN C15
