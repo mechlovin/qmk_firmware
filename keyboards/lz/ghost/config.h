ae65c81d34c025a08b64035b7f025eed1304cc24
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-05-01 20:18:06 +0700

    update

--- keyboards/lz/ghost/config.h
+++ keyboards/lz/ghost/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x6C7A
-#define PRODUCT_ID   0x0000
+#define PRODUCT_ID   0x0002
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Mechlovin Studio
 #define PRODUCT      Ghost
@@ -63,7 +63,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-#define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+#define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
 #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 /*== all animations enable ==*/
 #define RGBLIGHT_ANIMATIONS
