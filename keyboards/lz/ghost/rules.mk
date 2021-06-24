3fea03be102375689793a6cb81cf74c263458fc9
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-06-24 22:41:49 +0700

    Update rules.mk

--- keyboards/lz/ghost/rules.mk
+++ keyboards/lz/ghost/rules.mk
@@ -1,10 +1,9 @@
 # MCU name
-MCU = atmega32a
+MCU = atmega32u4
 
-F_CPU ?= 16000000
 
 # Bootloader selection
-BOOTLOADER = USBasp
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
@@ -12,12 +11,12 @@ BOOTLOADER = USBasp
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
+NKRO_ENABLE = yes            # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
