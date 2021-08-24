ca49c6d5337858df99502951bdbb5bda65f40544
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-08-24 10:41:28 +0700

    add hannah65-rev2

--- keyboards/mechlovin/hannah65/hannah65.h
+++ keyboards/mechlovin/hannah65/hannah65.h
@@ -20,6 +20,6 @@
 
 #if defined(KEYBOARD_mechlovin_hannah65_rev1)
 #    include "rev1.h"
-#elif defined(KEYBOARD_mechlovin_hannah65_mechlovin9)
-#    include "mechlovin9.h"
+#elif defined(KEYBOARD_mechlovin_hannah65_rev2)
+#    include "rev2.h"
 #endif
