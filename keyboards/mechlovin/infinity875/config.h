2fbf6e3915a06b14225f0980b8d0481e0523cafa
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-11-09 15:02:00 +0700

    update

--- keyboards/mechlovin/infinity875/config.h
+++ keyboards/mechlovin/infinity875/config.h
@@ -45,32 +45,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION ROW2COL
 
-#define USB_POLLING_INTERVAL_MS 1
-
 #define LED_CAPS_LOCK_PIN D6
 #define LED_SCROLL_LOCK_PIN B4
 #define LED_PIN_ON_STATE 1
 
-#ifdef BACKLIGHT_ENABLE
-#define BACKLIGHT_PIN B6
-#define BACKLIGHT_LEVELS 3
-#define BACKLIGHT_BREATHING
-#endif 
-
 #ifdef RGB_MATRIX_ENABLE
 #define RGB_DI_PIN E2
 #define DRIVER_LED_TOTAL 42
-#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-#define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_PINWHEEL // Sets the default mode, if none has been set
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#define DISABLE_RGB_MATRIX_SPLASH
-#define DISABLE_RGB_MATRIX_SOLID_SPLASH
 #define VIA_QMK_RGBLIGHT_ENABLE
 #endif
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
