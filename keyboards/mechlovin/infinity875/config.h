f039f7f1ce730787379d8cacc64990378266b68b
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-11-14 12:47:41 +0700

    update effect

--- keyboards/mechlovin/infinity875/config.h
+++ keyboards/mechlovin/infinity875/config.h
@@ -56,11 +56,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_PINWHEEL // Sets the default mode, if none has been set
 #define VIA_QMK_RGBLIGHT_ENABLE
+#define DISABLE_RGB_MATRIX_ALPHAS_MODS	
+#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN	
+#define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT	
+#define DISABLE_RGB_MATRIX_BREATHING	
+#define DISABLE_RGB_MATRIX_BAND_SAT	
+#define DISABLE_RGB_MATRIX_BAND_VAL	
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT	
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL	
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT	
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL	
+#define DISABLE_RGB_MATRIX_CYCLE_ALL	
+#define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT	
+#define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN	
+#define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON	
+#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN	
+#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL	
+#define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL	
+#define DISABLE_RGB_MATRIX_CYCLE_SPIRAL	
+#define DISABLE_RGB_MATRIX_DUAL_BEACON	
+#define DISABLE_RGB_MATRIX_RAINBOW_BEACON	
+#define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS	
+#define DISABLE_RGB_MATRIX_RAINDROPS	
+#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS	
+#define DISABLE_RGB_MATRIX_HUE_BREATHING	
+#define DISABLE_RGB_MATRIX_HUE_PENDULUM	
+#define DISABLE_RGB_MATRIX_HUE_WAVE	
 #endif
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
 
 
 /* Bootmagic Lite key configuration */
