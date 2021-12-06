82719e2050b979d9fe352cd6c44c71e4cc90739b
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-12-06 12:37:55 +0700

    change f303 to f103

--- keyboards/mechlovin/hannah60rgb/rev2/rules.mk
+++ keyboards/mechlovin/hannah60rgb/rev2/rules.mk
@@ -1,3 +1,10 @@
+# MCU name
+MCU = STM32F103
+
+# Bootloader selection
+BOOTLOADER = stm32duino
+
+
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3731
