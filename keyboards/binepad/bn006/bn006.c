c431d6951a040544b091327fe5bc4090354b4dea
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-03-04 14:44:36 +0700

    add bnd006

---
+++ keyboards/binepad/bn006/bn006.c
@@ -0,0 +1,31 @@
+/* Copyright 2020 Team Mechlovin'
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "bn006.h"
+
+
+led_config_t g_led_config = { {
+  //Key Matrix to LED Index
+  {1,         2,         3},
+  {66,        57,        67}
+}, {
+  //LED Index to Physical Positon
+  {0,0},      { 17,  0},  { 34, 0 },
+  {0,16},     { 17, 16},  { 34,16 },
+}, {
+  1,          1,          1,
+  1,          1,          1,
+} };
