128e5f409a7e4a73d03f48b86f436c373978a527
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-07-15 15:28:13 +0700

    update

--- keyboards/mechlovin/hex4b/singa/config.h
+++ keyboards/mechlovin/hex4b/singa/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { B11, B10, B2, B1, B0, A6, A5, A4, A2, C13, B7, B6, B5, B4, B3 }
 #define UNUSED_PINS
 
-
+#ifdef RGBLIGHT_ENABLE
 #define RGB_DI_PIN A7
 #define WS2812_SPI SPID1 // default: SPID1
 #define WS2812_SPI_MOSI_PAL_MODE 5 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
@@ -58,6 +58,46 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-
 #define RGBLIGHT_LAYERS
+#endif
 
+#ifdef RGB_MATRIX_ENABLE
+#define RGB_DI_PIN A7
+#define DRIVER_LED_TOTAL 3
+#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#define RGB_MATRIX_KEYPRESSES
+#undef ENABLE_RGB_MATRIX_SPLASH
+#undef ENABLE_RGB_MATRIX_MULTISPLASH
+#undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+// RGB Matrix Animation modes. Explicitly enabled
+// For full list of effects, see:
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    define ENABLE_RGB_MATRIX_BREATHING
+#    define ENABLE_RGB_MATRIX_BAND_SAT
+#    define ENABLE_RGB_MATRIX_BAND_VAL
+#    define ENABLE_RGB_MATRIX_CYCLE_ALL
+#    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#    define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#    define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#    define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#    define ENABLE_RGB_MATRIX_DUAL_BEACON
+#    define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#    define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#    define ENABLE_RGB_MATRIX_RAINDROPS
+#    define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#    define ENABLE_RGB_MATRIX_HUE_BREATHING
+#    define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#    define ENABLE_RGB_MATRIX_HUE_WAVE
+#    define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#    define ENABLE_RGB_MATRIX_PIXEL_FLOW
+#    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+//#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_ALL
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#define VIA_QMK_RGBLIGHT_ENABLE
+#endif
--- keyboards/mechlovin/hex4b/singa/singa.c
+++ keyboards/mechlovin/hex4b/singa/singa.c
@@ -15,7 +15,40 @@
  */
 
 #include "hex4b.h"
+#define XX NO_LED
 
+#ifdef RGB_MATRIX_ENABLE
+led_config_t g_led_config = { {
+  //Key Matrix to LED Index
+  { 0,  1,  2, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX},
+  {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX},
+  {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX},
+  {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX},
+  {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX},
+  {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX}
+}, {
+  //LED Index to Physical Positon
+  {0,0},  {14,0},    {28,0}
+}, {
+      1,       1,         1,
+} };
+
+__attribute__((weak)) void rgb_matrix_indicators_user(void) {
+        if (host_keyboard_led_state().caps_lock) {
+            rgb_matrix_set_color(2, 255, 255, 255);
+        }
+        if (host_keyboard_led_state().num_lock) {
+            rgb_matrix_set_color(1, 255, 255, 255);
+        }
+        if (host_keyboard_led_state().scroll_lock) {
+            rgb_matrix_set_color(0, 255, 255, 255);
+        }
+}
+
+#endif
+
+
+#ifdef RGBLIGHT_ENABLE
 // RGB LED Indicators
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {2, 1, HSV_WHITE}     // Light 1 LED, starting with LED 0
@@ -46,3 +79,4 @@ bool led_update_kb(led_t led_state) {
     rgblight_set_layer_state(2, led_state.scroll_lock);
     return true;
 }
+#endif
