28be39a59c44d980ce201b7e6cba5cd24e8c20a7
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-08-25 19:52:58 +0700

    Update keymap.c

--- keyboards/mechlovin/adelais/keymaps/via/keymap.c
+++ keyboards/mechlovin/adelais/keymaps/via/keymap.c
@@ -60,13 +60,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [2] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [3] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
-};
+    #ifdef KEYBOARD_mechlovin_adelais_rgb_led
+        const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+            [0] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [1] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [2] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [3] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
+        };
+    #elif KEYBOARD_mechlovin_adelais_standard_led
+        const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+            [0] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [1] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [2] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+            [3] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
+        };
+    #endif
 #endif
+
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
