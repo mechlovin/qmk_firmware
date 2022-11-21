11c9837b927d256e0dbd7aa7387bac195c854728
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-11-21 14:41:37 +0700

    update keyboard.h

--- keyboards/mechlovin/zed65/zed65.h
+++ keyboards/mechlovin/zed65/zed65.h
@@ -25,6 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #include "wearhaus66.h"
 #elif KEYBOARD_mechlovin_zed65_no_backlight_cor65
     #include "cor65.h"
+#elif KEYBOARD_mechlovin_zed65_no_backlight_cassini
+    #include "cassini.h"
 #elif KEYBOARD_mechlovin_zed65_mono_led
     #include "mono_led.h"
 #endif
