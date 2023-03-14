7a7eab01583ddcfbe00b68f7b9920eb873ee1811
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-03-14 10:32:34 +0700

    update

--- drivers/led/issi/is31fl3218.c
+++ drivers/led/issi/is31fl3218.c
@@ -74,8 +74,8 @@ void IS31FL3218_init(void) {
 }
 
 void IS31FL3218_set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
-    g_pwm_buffer[index * 3 + 0]  = red;
-    g_pwm_buffer[index * 3 + 1]  = green;
+    g_pwm_buffer[index * 3 + 1]  = red;
+    g_pwm_buffer[index * 3 + 0]  = green;
     g_pwm_buffer[index * 3 + 2]  = blue;
     g_pwm_buffer_update_required = true;
 }
