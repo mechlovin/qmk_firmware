617ed0d8d8ff5876ab13365d6f55f8b2ab316a37
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-06-18 12:25:44 +0700

    update indicator rgb on anc rev3
    follow Gondolindrim code.
    https://github.com/Gondolindrim/qmk_firmware/blob/apollo_indicator/keyboards/acheron/apollo/87h/delta/delta.c

--- keyboards/mechlovin/adelais/rgb_led/rev3/config.h
+++ keyboards/mechlovin/adelais/rgb_led/rev3/config.h
@@ -110,3 +110,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 10
+
+// EECONFIG data size: each indicator takes 5 bytes; there are four possible indicators.
+#define EECONFIG_KB_DATA_SIZE (4*6)
+
+#define WEAR_LEVELING_LOGICAL_SIZE 2048
+#define WEAR_LEVELING_BACKING_SIZE 4096	
