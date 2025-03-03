c724bba8434be90f4591c2e16c5827e474ad0e2f
mechlovin <57231893+mechlovin@users.noreply.github.com>
2025-03-03 12:19:27 +0700

    add iso layout

--- keyboards/jjw/cloudline2/cloudline2.c
+++ keyboards/jjw/cloudline2/cloudline2.c
@@ -19,12 +19,12 @@
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = { {
   //Key Matrix to LED Index
-  {NO_LED,            NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,      NO_LED,     NO_LED,     NO_LED},
-  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,      NO_LED,     NO_LED,     NO_LED},
-  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,      NO_LED,     NO_LED,     NO_LED},
-  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,                   NO_LED,                                    },
-  {NO_LED,            NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,                   NO_LED,                  NO_LED,           },
-  {NO_LED, NO_LED,    NO_LED,                                         NO_LED,                                          NO_LED,     NO_LED,      NO_LED,                   NO_LED,     NO_LED,     NO_LED}
+  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,         NO_LED,     NO_LED,     NO_LED},
+  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,         NO_LED,     NO_LED,     NO_LED},
+  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,         NO_LED,     NO_LED,     NO_LED},
+  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,                   NO_LED,                                       },
+  {NO_LED, NO_LED,    NO_LED,      NO_LED,     NO_LED,    NO_LED,     NO_LED,     NO_LED,     NO_LED,     NO_LED,      NO_LED,     NO_LED,      NO_LED,      NO_LED,                     NO_LED,           },
+  {NO_LED, NO_LED,    NO_LED,                                         NO_LED,                                          NO_LED,     NO_LED,      NO_LED,                      NO_LED,     NO_LED,     NO_LED}
 }, {
         {224,  0},    {196,  0},     {168,  0},     {140,  0},     {112,  0},     {84,  0},     {56,  0},     {28,  0},      {0,  0}, {28, 0},    {56, 0},    {84,  0},    {112,  0},    {140,  0},    {168,  0},    {196,  0},  {224,  0}   
 
